dcca02bb40ba7c28f99c633f91ade1d041ea05b7
Ozodbekyum <hello7ozod@gamil.com>
2022-07-21 15:34:29 +0500

    arith09

--- bin/arithmetic_operator09.dart
+++ bin/arithmetic_operator09.dart
@@ -9,5 +9,11 @@ Find the expression value assign to 'answer'. a+b - cd .
 
 */
 void main() {
+  int a=8;
+  int b=3;
+  int c=2;
+  int d=4;
+  int answer= a + b - c * d;
+  print(answer);
   //write your code here
 }
